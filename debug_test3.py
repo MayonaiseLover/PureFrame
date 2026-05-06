dd2fe65d6a06521a6ffc40797522de013e2fcead
Mazen Haitham <mazenelhwarey@gmail.com>
2026-05-07 00:17:24 +0300

    Phase 6: Polish, cleanup cruft, add .gitignore, fix pyproject.toml for PyPI

--- debug_test3.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-cap = cv2.VideoCapture('/tmp/debug_out.mp4')
-cap.set(cv2.CAP_PROP_POS_FRAMES, 150)
-ret, frame = cap.read()
-if ret:
-    print(np.unique(frame.reshape(-1, 3), axis=0))
